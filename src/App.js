20204a61e8887b044b968deb22188f8861f77dde
common-sense-cloud <kody@cloudology.dev>
2022-11-23 13:57:06 -0700

    updated tests

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn What CI/CD is
+          Learn What CICD is
         </a>
       </header>
     </div>
