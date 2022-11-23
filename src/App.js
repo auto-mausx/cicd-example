225aaa52b0c43f093db83a751732f3f4a096aa24
common-sense-cloud <kody@cloudology.dev>
2022-11-23 14:48:14 -0700

    revert

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Here's a sentance that makes no sense at all
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
